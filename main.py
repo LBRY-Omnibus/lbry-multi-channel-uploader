175d16873868948f2505d013b1be7141ff91cb96
no7mac <44577325+no7macs@users.noreply.github.com>
2022-01-31 19:40:50 -0700

    wallets and accounts are now definable

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from imgurpython import ImgurClient
-from requests.api import request
 import requests
 import json
 import os
 import sqlite3
 import time
 import hashlib
-import random
+import gc
 
 
 progPath = os.path.dirname(os.path.abspath(__file__))
 
 #grabs balance and unlocks supports is balance is insuficient
-def checkBal() -> str:
+def checkBal(wallet) -> str:
     print('--waiting to check balance--')
     time.sleep(90)
-    wallet = requests.post("http://localhost:5279", json={"method": "wallet_balance", "params": {"wallet_id": "nsfw"}}).json()
+    wallet = requests.post("http://localhost:5279", json={"method": "wallet_balance", "params": {"wallet_id": wallet}}).json()
     walletBal = float(wallet['result']['available'])
     print(walletBal)
 
     if(walletBal <= 1):
         print('--unlocking funds--')
-        supports = requests.post("http://localhost:5279", json={"method": "support_list", "params": {"wallet_id": "nsfw", "page_size": 50}}).json()
+        supports = requests.post("http://localhost:5279", json={"method": "support_list", "params": {"wallet_id": wallet, "page_size": 50}}).json()
         print(supports)
         for a in supports["result"]["items"]:
-            requests.post("http://localhost:5279", json={"method": "support_abandon", "params": {"claim_id": a['claim_id'], "wallet_id": "nsfw"}}).json()
-        checkBal()
+            requests.post("http://localhost:5279", json={"method": "support_abandon", "params": {"claim_id": a['claim_id'], "wallet_id": wallet}}).json()
+        #checkBal(wallet)
     else:
         return(walletBal)
     return(walletBal)
@@ -47,15 +46,15 @@ def getNotUploaded(channel, files) -> list:
     return(unUploaded)
 
 #add uploaded to db
-def insertNewUpload(channel, file, url) -> None:
-    curr.execute(f"""INSERT INTO uploaded(channel, file, url) VALUES('{channel}', '{file}', '{url}')""")
+def insertNewUpload(wallet, channel, file, url) -> None:
+    curr.execute(f"""INSERT INTO uploaded(wallet, channel, file, url) VALUES('{wallet}', '{channel}', '{file}', '{url}')""")
     dataBase.commit()
     return()
 
 #uploads thumbnail to imgur
 def imgurUploader(image) -> str:
     client_id = '30383064d011baa'
-    client_secret = '7d80b6d588a7e1b1b7b4f1ced3b83b13b6e51034'
+    client_secret = '220dedf34c01f3ce3fd14278819920a3adfec313'
     client = ImgurClient(client_id, client_secret)
     thumbnail = client.upload_from_path(image, config=None, anon=True)
     print(thumbnail['link'])
@@ -68,7 +67,7 @@ def thumbnailCreator(video) -> str:
      return(progPath + "/temp/thumbnailTemp.gif")
 
 #uploads video to lbry
-def uploadToLBRY(channels, channel, file) -> str:
+def uploadToLBRY(wallet, channels, channel, file, funding_account_ids, account_id) -> str:
     channelInfo = channels[channel]
     thumbnail = imgurUploader(thumbnailCreator(file))
     name = str(hashlib.sha256(os.path.splitext(file)[0].encode('utf-8').strip()).hexdigest())[:5] + str(os.path.splitext(os.path.basename(file))[0])
@@ -77,41 +76,45 @@ def uploadToLBRY(channels, channel, file) -> str:
     upload = requests.post("http://localhost:5279", json={"method": "publish", "params": {
                                 "name": name, 
                                 "bid": channelInfo['upload_fee'], "file_path": file, "title": os.path.splitext(os.path.basename(file))[0], "tags": channelInfo['content_tags'], 
-                                "thumbnail_url": thumbnail, "channel_id": channelInfo['channel_id'], "account_id": "bQnAHW3yQAMKrzMUrzevb6zPFDtHABhmXr", "wallet_id": "nsfw", 
-                                "funding_account_ids": ["bQnAHW3yQAMKrzMUrzevb6zPFDtHABhmXr", "bPqPneKocvBHye5aRGZE1E2qjKExjKKtYX"]}}).json()
-    #print(upload)
+                                "thumbnail_url": thumbnail, "channel_id": channelInfo['channel_id'], "account_id": account_id, "wallet_id": wallet, 
+                                "funding_account_ids": funding_account_ids}}).json()
     print(upload)
     if 'error' in upload.keys():
-        checkBal()
+        checkBal(wallet)
         afterError = uploadToLBRY(channels, channel, file)
         return(afterError)
     else:
-        insertNewUpload(channel, file, (upload["result"]["outputs"][0]["permanent_url"]))
+        insertNewUpload(wallet, channel, file, (upload["result"]["outputs"][0]["permanent_url"]))
         return(upload['result']['outputs'][0]['permanent_url'])
 
 if __name__ == "__main__":
     global dataBase, curr
-    for gayloop in range(0,1):
+    for gayloop in range(0,1): #shit hack for making bad code run once 
         dataBase = sqlite3.connect(progPath + '/db.s3db')
         curr = dataBase.cursor()
-        channels = getChannelList()
-        for a in channels:
-            print(a)
-            #allows for content to be spread across different folders
-            fileList = []
-            for d in (0, len(channels[a]['content_folder'])-1):
-                for (root,dirs,files) in os.walk(channels[a]['content_folder'][d], topdown=True, followlinks=True):
-                    if "ignore" in channels[a].keys():
-                        dirs[:] = [g for g in dirs if g not in channels[a]['ignore']]
-                    #combines root and name together if name does not have !qB in it and adds to list, otherwise add None (e), then removes every none in list (f) 
-                    fileList.extend(f for f in [(root.replace('\\', '/') + '/' + e) if '.!qB' not in e else None for e in files] if f)
-            notUploaded = getNotUploaded(a, fileList)
-            print(notUploaded)
-            for c in notUploaded[0:1]:
-                url = uploadToLBRY(channels, a, c)
-                print(url)
-            print("--Sleeping--")
-            time.sleep(30) if not len(notUploaded) == 0 else time.sleep(1)
+        lbryData = getChannelList()
+        for wallet in lbryData:
+            addWallet = requests.post("http://localhost:5279", json={"method": "wallet_add", "params": {'wallet_id':wallet}}).json()
+            for channel in lbryData[wallet]:
+                print(channel)
+                #allows for content to be spread across different folders
+                fileList = []
+                for d in (0, len(lbryData[wallet][channel]['content_folder'])-1):
+                    for (root,dirs,files) in os.walk(lbryData[wallet][channel]['content_folder'][d], topdown=True, followlinks=True):
+                        if "ignore" in lbryData[wallet][channel].keys():
+                            dirs[:] = [g for g in dirs if g not in lbryData[wallet][channel]['ignore']]
+                        #combines root and name together if name does not have !qB in it and adds to list, otherwise add None (e), then removes every none in list (f) 
+                        fileList.extend(f for f in [(root.replace('\\', '/') + '/' + e) if '.!qB' not in e else None for e in files] if f)
+                notUploaded = getNotUploaded(channel, fileList)
+                print(notUploaded)
+                for c in notUploaded[0:1]:
+                    url = uploadToLBRY(wallet, lbryData[wallet], channel, c, lbryData[wallet][channel]['funding_account_ids'], lbryData[wallet][channel]['account_id'])
+                    print(url)
+                print("--Sleeping--")
+                time.sleep(30) if not len(notUploaded) == 0 else time.sleep(1)
+            if not wallet == "default_wallet":
+                removeWallet = requests.post("http://localhost:5279", json={"method": "wallet_remove", "params": {'wallet_id':wallet}}).json()
+            gc.collect()
         dataBase.close()
         print("--Finnished Set, Sleeping--")
         time.sleep(30)
