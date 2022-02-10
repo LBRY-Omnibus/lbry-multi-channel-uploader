6f50fff0a1384a65a795a11c30d7512c955cd125
no7mac <44577325+no7macs@users.noreply.github.com>
2022-02-09 20:29:03 -0700

    removed upload because wack file issues

--- uploader/scripts/thumbnail/gifFirst3Sec/gifFirst3Sec.py
+++
@@ -1,6 +0,0 @@
-def main(video) -> str:
-    import os
-    progPath = os.path.dirname(os.path.abspath(__file__))
-    os.system(f"""ffmpeg -i "{video}" -fs 3M -vf "scale=640:-2" -y "{progPath + '/temp/thumbnailTemp.gif'}" """)
-    print(progPath + "/temp/thumbnailTemp.gif")
-    return(progPath + "/temp/thumbnailTemp.gif")
--- uploader/scripts/thumbnailUpload/imgur/imgur.py
+++
@@ -1,9 +0,0 @@
-from imgurpython import ImgurClient
-import os
-def main(image) -> str:
-    client_id = '30383064d011baa'
-    client_secret = '220dedf34c01f3ce3fd14278819920a3adfec313'
-    client = ImgurClient(client_id, client_secret)
-    thumbnail = client.upload_from_path(image, config=None, anon=True)
-    os.remove(image)
-    return(thumbnail['link'])
