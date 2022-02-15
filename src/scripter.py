f05032d78cd91b74db5872970561b078307bcad8
no7mac <bendavison949@gmail.com>
2022-02-15 10:27:19 -0700

    'in' and 'where' defined with 'in' mostly done, bug fix later

--- src/scripter.py
+++ src/scripter.py
@@ -11,38 +11,35 @@ curr = dataBase.cursor()
 with open('./script.json', 'r') as jsonRaw:
     jsonDat =  json.load(jsonRaw)
 
-channelName = 'vidya'
-channelDat = curr.execute(f"""SELECT * FROM channels WHERE channel_name = '{channelName}' """).fetchall()
-contentTags = curr.execute(f"""SELECT tag FROM content_tag WHERE channel_name = '{channelName}' """).fetchall()
-contentTags = [a[0] for a in contentTags]
-fundingAccounts = curr.execute(f"""SELECT account_id FROM funding_account WHERE channel_name = '{channelName}' """).fetchall()
-fundingAccounts = [a[0] for a in fundingAccounts]
-contentFolders = curr.execute(f"""SELECT folder FROM content_folder WHERE channel_name = '{channelName}' """).fetchall()
-contentFolders = [a[0] for a in contentFolders]
-
 for a in jsonDat:
     if a == 'upload':
+        # -----------------------------------------------------------------------------------------------
+        # 'in' grabs channels if they have the given data,
+        # the channels returned are used later for 'where' and uploading when they are eventually looped over
+        # if in is not suplied in the script then it defaults to all channels
+        # -----------------------------------------------------------------------------------------------
         if 'in' in list(jsonDat[a][1])[0]:
             query = "SELECT * FROM channels WHERE"
             for b in jsonDat[a][1]['in']:
-                if type(b) == dict and (dictHeader := list(b)[0]) in ['channel_name', 'channel_id', 'wallet_name', 'acount_id', 'upload_fee']:
-                    query += f" {dictHeader} = '{b[dictHeader]}'"
+                if type(b) == dict:
+                    dictHeader = list(b)[0]
+                    #includes channel with given name, id, wallet name, account id, or fee
+                    if dictHeader in ['channel_name', 'channel_id', 'wallet_name', 'acount_id', 'upload_fee']:
+                        query += f" {dictHeader} = '{b[dictHeader]}'"
+                    #includes channel with given folder, tag, ot funding account
+                    dictDataField = {'content_folder':'folder', 'content_tag': 'channel_name', 'funding_account':'channel_name'}
+                    if dictHeader in list(dictDataField):
+                        query += f" channel_name IN (SELECT channel_name FROM {dictHeader} WHERE {dictDataField[dictHeader]} = '{b[dictHeader]}')"
+                
                 elif type(b) == str and b in ['OR', 'AND', 'OR NOT', 'AND NOT']:
                     query += f" {b}"
         else:
             query = "SELECT * FROM channels"
-
         channelDat = curr.execute(query).fetchall()
-
-        #contentTags = curr.execute(f"""SELECT tag FROM content_tag WHERE channel_name = '{channelName}' """).fetchall()
-        #contentTags = [a[0] for a in contentTags]
-        #fundingAccounts = curr.execute(f"""SELECT account_id FROM funding_account WHERE channel_name = '{channelName}' """).fetchall()
-        #fundingAccounts = [a[0] for a in fundingAccounts]
-        #contentFolders = curr.execute(f"""SELECT folder FROM content_folder WHERE channel_name = '{channelName}' """).fetchall()
-        #contentFolders = [a[0] for a in contentFolders]
-
-        if 'where' in list(jsonDat[a][1])[0]:
-            for b in jsonDat[a][1]['where']:
-                pass
-            withResult = curr.execute(query).fetchall()
-            print(withResult)
+        # -----------------------------------------------------------------------------------------------
+        # while 'in' grabs channels if they have the given data,
+        # 'where' uses the data for each channel if the conditions say so
+        # -----------------------------------------------------------------------------------------------
+        if 'in' in list(jsonDat[a][1])[0]:
+            pass
+            #make soemtime soon please an thank future me
