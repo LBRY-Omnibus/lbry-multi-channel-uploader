b110238090cc630b24d7830c6ee423e5dddc290d
no7mac <44577325+no7macs@users.noreply.github.com>
2022-02-15 07:06:15 -0700

    small work on scripter and script system

--- src/scripter.py
+++ src/scripter.py
@@ -12,20 +12,13 @@ with open('./script.json', 'r') as jsonRaw:
     jsonDat =  json.load(jsonRaw)
 
 for a in jsonDat:
-    if 'in' in jsonDat[a][1].keys():
-        query = "SELECT * FROM channels WHERE"
-        for b in jsonDat[a][1]['in']:
-            print(jsonDat[a][1]['in'])
-            if type(b) == dict:
-                if 'channel' in b.keys():
-                    query += f" channel_name = '{b['channel']}'"
-                elif 'wallet' in b.keys():
-                    query += f" wallet_name = '{b['wallet']}'"
-                elif 'account' in b.keys():
-                    query += f" account_id = '{b['account']}'"
-            elif type(b) == str:
-                query += f" {b} "
-        print(query)
-        withResult = curr.execute(query).fetchall()
-        print(withResult)  
-            
+    if a == 'upload':
+        if 'in' in list(jsonDat[a][1])[0]:
+            query = "SELECT * FROM channels WHERE"
+            for b in jsonDat[a][1]['in']:
+                if type(b) == dict and (dictHeader := list(b)[0]) in ['channel_name', 'channel_id', 'wallet_name', 'acount_id', 'upload_fee']:
+                    query += f" {dictHeader} = '{b[dictHeader]}'"
+                elif type(b) == str and b in ['OR', 'AND', 'OR NOT', 'AND NOT']:
+                    query += f" {b}"
+            withResult = curr.execute(query).fetchall()
+            print(withResult)
